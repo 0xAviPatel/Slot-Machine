5cf60062a6d0c7d10f8a97b2198ca78de37d5106
Avi <avip3513@gmail.com>
2022-09-25 19:02:58 +0530

    Slot Machine App

---
+++ src/components/SlotMachine.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const SlotMachine = (props) => {
+  //   const x = "😀";
+  //   const y = "😀";
+  // //   const z = "😀";
+  //   const z = "😁";
+
+  console.log(props);
+
+  const x = props.x;
+  const y = props.y;
+  const z = props.z;
+
+  return (
+    <>
+      <div>
+        {x} {y} {z}
+      </div>
+      <div>{x === y && y === z ? "It is matched" : "It is not matched"}</div>
+      <hr />
+    </>
+  );
+};
+
+export default SlotMachine;
